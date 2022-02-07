e6ee2f48584ac3e0977219ad6070aee4ee5fccb9
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-02-07 15:27:43 -0800

    finchcollector part 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Finch(models.Model):
@@ -7,5 +9,12 @@ class Finch(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
 
+    class Meta: 
+        verbose_name = 'finch'
+        verbose_name_plural = 'finches'
+
     def __str__(self):
         return f'{self.name}, ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Create your views here.
@@ -18,3 +19,17 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', { 'finch': finch})
+
+#class based view
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['type', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
+
