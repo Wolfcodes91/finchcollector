44a4d2a0907920e91d462b3e5edeabb6bcc07f6b
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-02-07 14:42:24 -0800

    finchcollector part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,20 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
-from django.http import HttpResponse
 
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, type, description, age):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Ryland', 'saffron', 'chill, naps a lot',  3),
-  Finch('Mehj', 'blue', 'pretty bird, sips tea', 2),
-  Finch('Dave', 'spice', 'spicy-boy, charming', 4),
-  Finch('Devlin', 'society', 'high-society', 4)
-]
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>FinchCollections</h1>')
+  return render(request, 'home.html')
 
 def about(request):
    return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch})
